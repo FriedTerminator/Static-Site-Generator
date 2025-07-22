e5ab29fc853f0c02a75d200919db8b697ca5f124
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-07-22 12:21:47 -0700

    added some more code to generating page

--- src/page_creation.py
+++ src/page_creation.py
@@ -3,11 +3,12 @@ from blocktype import markdown_to_html_node
 
 def extract_title(markdown):
     header = re.findall("\# (.*?)", markdown)
-    if header == None:
+    if header == []:
         raise Exception("No header found")
-    return header.strip("# ")
+    return header[0]
 
 def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as f:
         markdown_content = f.read()
@@ -17,6 +18,12 @@ def generate_page(from_path, template_path, dest_path):
 
     html_node = markdown_to_html_node(markdown_content)
     html = html_node.to_html()
-    header = extract_title(html)
+    title = extract_title(markdown_content)
+
+    updated_html = template_content.replace("{{ Title }}", title)
+    updated_html = updated_html.replace("{{ Content }}", html)
+
+
+
 
     
